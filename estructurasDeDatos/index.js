0727fa5c1de2952de8d91408833dbd1fd40bae6c
donCelis <alexcelis019@gmail.com>
2023-06-05 20:55:16 -0500

    configurar el lintern con stantard JS

--- estructurasDeDatos/index.js
+++ estructurasDeDatos/index.js
@@ -1,1 +1,32 @@
-console.log("Hola mundo!")
+// Como pasar de una clase a una función
+class Person {
+  constructor (firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.fullName = `${firstName} ${lastName}`
+  }
+
+  hi () {
+    return `Hola ${this.fullName}`
+  }
+}
+
+const Hugo = new Person('Hugo', 'Prieto')
+
+console.log(Hugo.hi())
+
+function PersonFunc (firstName, lastName) {
+  const fullName = `${firstName} ${lastName}`
+
+  function hi () {
+    return `Hola ${fullName}`
+  }
+
+  return {
+    hi
+  }
+}
+
+const Estefania = PersonFunc('Estefania', 'Hernandez')
+
+console.log(Estefania.hi())
