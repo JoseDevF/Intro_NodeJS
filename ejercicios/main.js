15095f28c44f882d254ae850e66e85c992cdb7bb
JoseHerCast <jose@ptree.com.mx>
2023-06-19 19:48:08 -0600

    MVP de CRUD de Jedis

--- ejercicios/main.js
+++ ejercicios/main.js
@@ -1,11 +1,14 @@
+import arregloJedis from './data/mockupJedis.js';
+
 /* 
 Rango:
 
-Maestro de la Orden[3]
-Maestro Jedi[2]
-Caballero Jedi[2]
-Padawan[2]
-Iniciado Jedi[4]
+Maestro de la Orden: 5
+Maestro Jedi: 4
+Caballero Jedi: 3
+Padawan: 2
+Iniciado Jedi: 1
+
 
 Estadisticas:
 
@@ -15,39 +18,9 @@ Salud
 */
 
 /* Arreglo de jedis inicializado en un arreglo vacío */
-const arregloJedis = []
+/* const arregloJedis = [] */
 
-/*Obteniendo el formulario desde el DOM (desde el html), a través del ID*/
-const form = document.querySelector('#characterRegistry');
-
-/* Manejador de evento */
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Evita que se envíe el formulario automáticamente
-
-    /* Almacenando la información del formulario, en un objeto de javascript */
-    const character = {
-        name: form.name.value,
-        age: Number(form.age.value),
-        team: form.team.value
-    };
-
-    if (validateCharacter(character)) {
-        arregloJedis.push(character);
-        updateJediList();
-        console.log(arregloJedis);
-    } else {
-        alert("Tienes un dato vacío, verificalo");
-        event.stopPropagation();
-    }
-})
-
-/*
-Maestro de la Orden: 5
-Maestro Jedi: 4
-Caballero Jedi: 3
-Padawan: 2
-Iniciado Jedi: 1
-*/
+/* Declaraciones */
 
 const setRole = (jedi, role) => {
     const roles = {
@@ -56,68 +29,208 @@ const setRole = (jedi, role) => {
         3: "Caballero Jedi",
         2: "Padawan",
         1: "Iniciado Jedi"
-    }
+    };
+
     const validRole = roles[role];
-    console.log(validRole);
+
     if (validRole) {
+        // Si el rol es válido, se asigna al jedi
         return {
             ...jedi,
             role: validRole
-        }
+        };
     } else {
+        // Si el rol no es válido, se asigna "Desconocido" al jedi
         return {
             ...jedi,
             role: "Desconocido"
+        };
+    }
+};
+
+const handleFormSubmit = (event) => {
+    event.preventDefault(); // Evita que se envíe el formulario automáticamente
+
+    /* Almacenando la información del formulario en un objeto de JavaScript */
+    const character = {
+        name: form.name.value,
+        age: Number(form.age.value),
+        team: form.team.value
+    };
+
+    /*TODO: Valide tambien que el nombre del jedi no este repetido */
+    if (validateCharacter(character)) {
+        // Si los datos del personaje son válidos, se agrega al arreglo de jedis y se actualiza la lista
+        arregloJedis.push(setRole(character, 1));
+        updateJediList();
+        console.log(arregloJedis);
+    } else {
+        // Si hay datos vacíos, se muestra una alerta y se detiene la propagación del evento
+        alert("Tienes un dato vacío, verifícalo");
+        event.stopPropagation();
+    }
+};
+
+const handleChangeRole = () => {
+    /* TODO: Mejora que se tiene que hacer:
+        -Pedir el nombre
+        -Guardar el indice con un click en la fila del CRUD
+    */
+    const index = prompt('Ingrese el índice del jedi:', '');
+    /* Que pasa si me da el nombre ¿? */
+    const jedi = arregloJedis[index];
+
+    if (jedi) {
+        /* TODO: Utilizar una modal de bootstrap */
+        const newRole = prompt('Ingrese el nuevo rol:', '');
+
+        if (newRole >= 1 && newRole <= 5) {
+            // Si el nuevo rol es válido, se actualiza el rol del jedi en el arreglo de jedis
+            arregloJedis[index] = setRole(jedi, Number(newRole));
+
+            updateJediList();
+            console.log(arregloJedis);
+        } else {
+            alert('Rol inválido');
+        }
+    } else {
+        alert('Índice inválido');
+    }
+};
+
+const handleEditJedi = (event) => {
+    const index = event.target.dataset.index;
+    const jedi = arregloJedis[index];
+    /* TODO: Utilizar formulario en una modal de bootstrap */
+    const newName = prompt('Ingrese el nuevo nombre:', jedi.name);
+    const newAge = prompt('Ingrese la nueva edad:', jedi.age);
+    const newTeam = prompt('Ingrese el nuevo equipo:', jedi.team);
+
+    if (newName && newAge && newTeam) {
+        // Si se proporcionan nuevos valores válidos, se actualiza el jedi en el arreglo de jedis
+        arregloJedis[index] = {
+            ...jedi,
+            name: newName,
+            age: Number(newAge),
+            team: newTeam
+        };
+
+        updateJediList();
+        console.log(arregloJedis);
+    }
+};
+
+const handleDeleteJedi = (event) => {
+    const index = event.target.dataset.index;
+    arregloJedis.splice(index, 1);
+
+    updateJediList();
+    console.log(arregloJedis);
+};
+
+const handleSortJedis = () => {
+    // Implementar el algoritmo de ordenamiento por selección aquí
+    selectionSort(arregloJedis);
+
+    // Actualizar la lista de jedis en el DOM
+    updateJediList();
+};
+
+const handleFilterJedis = () => {
+    const filterValue = jediNameFilter.value.toLowerCase().trim();
+
+    // Filtrar los jedis basado en el valor del input de búsqueda
+    const filteredJedis = arregloJedis.filter((jedi) =>
+        jedi.name.toLowerCase().includes(filterValue)
+    );
+
+    // Actualizar la lista de jedis filtrados en el DOM
+    updateJediList(filteredJedis);
+};
+
+const selectionSort = (array) => {
+    const length = array.length;
+
+    // Algoritmo de ordenamiento por selección
+    for (let i = 0; i < length - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < length; j++) {
+            if (array[j].name < array[minIndex].name) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            swap(array, i, minIndex);
         }
     }
-}
+};
 
-/* Validamos que no haya propiedades vacías */
-const validateCharacter = ({ name = "", age = 0, team = "" }) => name !== "" && age !== 0 && team !== ""; //Regresa un booleano
+const swap = (array, index1, index2) => {
+    // Intercambiar elementos de posición en el arreglo
+    const temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+};
 
-
-const updateJediList = () => {
+/* Función que crea el CRUD */
+const updateJediList = (filteredJedis = arregloJedis) => {
     const jediList = document.querySelector('#jediList');
     jediList.innerHTML = ''; // Limpiamos el contenido existente
 
-    arregloJedis.forEach(jedi => {
+    filteredJedis.forEach((jedi, index) => {
         const row = `
         <tr>
           <td>${jedi.name}</td>
           <td>${jedi.age}</td>
           <td>${jedi.team}</td>
+          <td>${jedi.role}</td>
+          <td>
+            <button type="button" class="btn btn-warning editBtn" data-index="${index}">Editar</button>
+            <button type="button" class="btn btn-danger deleteBtn" data-index="${index}">Eliminar</button>
+          </td>
         </tr>
       `;
 
         jediList.innerHTML += row;
     });
+
+    // Agregar eventos a los botones de edición y eliminación
+    const editButtons = document.querySelectorAll('.editBtn');
+    const deleteButtons = document.querySelectorAll('.deleteBtn');
+
+    editButtons.forEach((button) => {
+        button.addEventListener('click', handleEditJedi);
+    });
+
+    deleteButtons.forEach((button) => {
+        button.addEventListener('click', handleDeleteJedi);
+    });
 };
 
+/* Validamos que no haya propiedades vacías */
+const validateCharacter = ({ name = "", age = 0, team = "" }) => name !== "" && age !== 0 && team !== ""; // Regresa un booleano
 
-// Función para actualizar la lista de Jedis en el cuerpo de la tabla
-/* const updateJediList = () => {
-    const jediList = document.querySelector('#jediList');
-    jediList.innerHTML = ''; // Limpiamos el contenido existente
+/* Ejecución */
 
-    arregloJedis.forEach(jedi => {
-        const row = document.createElement('tr');
+updateJediList();
 
-        const nameCell = document.createElement('td');
-        nameCell.textContent = jedi.name;
+/*Obteniendo el formulario desde el DOM (desde el html), a través del ID*/
+const form = document.querySelector('#characterRegistry');
 
-        const ageCell = document.createElement('td');
-        ageCell.textContent = jedi.age;
+/* Manejador de evento */
+form.addEventListener('submit', handleFormSubmit)
 
-        const teamCell = document.createElement('td');
-        teamCell.textContent = jedi.team;
+// Agregar evento al botón de cambiar rol
+const changeRoleBtn = document.querySelector('#changeRoleBtn');
+changeRoleBtn.addEventListener('click', handleChangeRole);
 
-        row.appendChild(nameCell);
-        row.appendChild(ageCell);
-        row.appendChild(teamCell);
+// Agregar evento al botón de ordenar
+const sortButton = document.querySelector('#orderButton');
+sortButton.addEventListener('click', handleSortJedis);
 
-        jediList.appendChild(row);
-
-    });
-}; */
-
+//Agregar evento al campo de entrada del filtro por nombre
+const jediNameFilter = document.querySelector('#jediNameFilter');
+jediNameFilter.addEventListener('input', handleFilterJedis);
 
